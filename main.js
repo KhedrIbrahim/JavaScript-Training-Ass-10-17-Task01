618502dedec0f2292a10c4681bc383d0e9a8b5d7
Khedr Ibrahim <108041341+KhedrIbrahim@users.noreply.github.com>
2023-01-04 06:13:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// Add Variables Here
+let NumberOne = 10;
+    NumberTwo = 20;
+
+// Comment is Ouput
+console.log(NumberOne+""+NumberTwo); // Normal Concatenate => 1020
+console.log(typeof (NumberOne+""+NumberTwo)); // Normal Concatenate => String
+console.log(`${NumberOne}${NumberTwo}`); // Template Literals Way => 1020
+console.log(typeof(`${NumberOne}${NumberTwo}`)); // Template Literals Way => String
+
+console.log(NumberTwo +"\n"+ NumberOne);
+/*
+    Normal Concatenate
+    20
+    10
+*/
+
+console.log(`${NumberTwo}
+${NumberOne}`);
+/*
+    Template Literals Way
+    20
+    10
+*/
